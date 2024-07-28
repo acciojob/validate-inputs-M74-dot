902253f61a3c85f824f82d672e695d2009395565
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-28 15:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.getElementById('myform').addEventListener('submit', function(event){
+	event.preventDefault();
+	
+	var name = document.getElementById('name').value;
+	var email = document.getElementById('email').value;
+	var phone = document.getElementById('phone').value;
+
+	var namePtr = /^[A-Za-z]{3,}$/;
+	var emailPtr = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+	var phonePtr = /^[0-9]{10}$/;
+
+	document.getElementById('name').style.backgroundColor = namePtr.test(name) ? "lightgreen" : "pink";
+	document.getElementById('email').style.backgroundColor = emailPtr.test(email)? "lightgreen":"pink";
+	document.getElementById('phone').style.backgroundColor = phonePtr.test(phone)?"lightgreen":"pink";
+	
+	
+});
